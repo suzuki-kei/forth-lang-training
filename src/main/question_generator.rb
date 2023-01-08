f7dd6cd9d9ac31f47bb67d9d6af702e6f18e02b2
SUZUKI Kei <todokimasen@gmail.com>
2023-01-09 07:54:56 +0900

    複数の乱数を生成するメソッドを追加した.

--- src/main/question_generator.rb
+++ src/main/question_generator.rb
@@ -18,33 +18,34 @@ class QuestionGenerator
         (rand * 8).to_i + 1
     end
 
+    def random_integers(n)
+        n.times.map do
+            self.random_integer
+        end
+    end
+
     def generate_question_addition
-        x1 = self.random_integer
-        x2 = self.random_integer
+        x1, x2 = self.random_integers(2)
         Question.new("#{x1} #{x2}", "#{x1 + x2}", '+')
     end
 
     def generate_question_subtract
-        x1 = self.random_integer
-        x2 = self.random_integer
+        x1, x2 = self.random_integers(2)
         Question.new("#{x1} #{x2}", "#{x1 - x2}", '-')
     end
 
     def generate_question_multiple
-        x1 = self.random_integer
-        x2 = self.random_integer
+        x1, x2 = self.random_integers(2)
         Question.new("#{x1} #{x2}", "#{x1 * x2}", '*')
     end
 
     def generate_question_division
-        x1 = self.random_integer
-        x2 = self.random_integer
+        x1, x2 = self.random_integers(2)
         Question.new("#{x1 * x2} #{x1}", "#{x2}", '/')
     end
 
     def generate_question_swap
-        x1 = self.random_integer
-        x2 = self.random_integer
+        x1, x2 = self.random_integers(2)
         Question.new("#{x1} #{x2}", "#{x2} #{x1}", 'swap')
     end
 
