688757aa2d2c7caeb37f358e9731a175c1051c7f
SUZUKI Kei <todokimasen@gmail.com>
2023-01-09 10:26:51 +0900

    アプリケーションの処理本体を Application クラスに切り出した.

---
+++ src/main/application.rb
@@ -0,0 +1,46 @@
+require 'question_generator'
+
+class Application
+
+    def main
+        mode = self.parse_arguments
+        generator = QuestionGenerator.new(mode: mode)
+        self.question_loop(generator)
+    end
+
+    private
+
+    def parse_arguments
+        if !(ARGV.size <= 2)
+            $stderr.puts "Invalid options: [#{ARGV[1..-1].join(' ')}]."
+            exit 1
+        end
+
+        mode = case ARGV[1]
+            when nil
+                :shuffle
+            when '--sample'
+                :sample
+            when '--shuffle'
+                :shuffle
+            else
+                $stderr.puts "Invalid mode: [#{ARGV[1]}]."
+                exit 1
+        end
+
+        mode
+    end
+
+    def question_loop(question_generator)
+        question_generator.each do |question|
+            print "#{question} => "
+            line = $stdin.readline
+            question.answer(line)
+        end
+    rescue EOFError => e
+        # 入力が終了した場合はプログラムを終了する.
+        puts
+    end
+
+end
+
--- src/main/main.rb
+++ src/main/main.rb
@@ -1,42 +1,4 @@
-require 'question_generator'
+require 'application'
 
-def main
-    mode = parse_arguments
-    generator = QuestionGenerator.new(mode: mode)
-    question_loop(generator)
-end
-
-def parse_arguments
-    if !(ARGV.size <= 2)
-        $stderr.puts "Invalid options: [#{ARGV[1..-1].join(' ')}]."
-        exit 1
-    end
-
-    mode = case ARGV[1]
-        when nil
-            :shuffle
-        when '--sample'
-            :sample
-        when '--shuffle'
-            :shuffle
-        else
-            $stderr.puts "Invalid mode: [#{ARGV[1]}]."
-            exit 1
-    end
-
-    mode
-end
-
-def question_loop(question_generator)
-    question_generator.each do |question|
-        print "#{question} => "
-        line = $stdin.readline
-        question.answer(line)
-    end
-rescue EOFError => e
-    # 入力が終了した場合はプログラムを終了する.
-    puts
-end
-
-main
+Application.new.main
 
